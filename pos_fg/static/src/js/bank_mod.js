b47377a17157bbcb5d2751af366712a9a8ed4770
LaylaBahloul <80261579+LaylaBahloul@users.noreply.github.com>
2021-04-19 13:02:12 +0300

    Update bank_mod.js

--- pos_fg/static/src/js/bank_mod.js
+++ pos_fg/static/src/js/bank_mod.js
@@ -4,7 +4,7 @@ odoo.define('cash',function(require){
     let PaymentScreen = require('point_of_sale.PaymentScreen');
     const originalAddNewPaymentLine = PaymentScreen.prototype.addNewPaymentLine;
     PaymentScreen.prototype.addNewPaymentLine = function ({ detail: paymentMethod }) {
-        if (paymentMethod.name == 'Bank') {
+        if (paymentMethod.name == 'Credit Card'||paymentMethod.name == 'M-Pesa' ) {
            let auth_code_input = document.querySelector('input[name="auth_code"]');
            auth_code_input.disabled = false;
            auth_code_input.style.backgroundColor = "white";
