05c2d317e94ca1873628493b96c6bed241805c25
mmbashiti <mmbashiti@gmail.com>
2021-05-30 22:03:43 +0300

    Update product_form.py

--- madfox_fashiongala/models/product_form.py
+++ madfox_fashiongala/models/product_form.py
@@ -21,7 +21,7 @@ class Madfox_Product(models.Model):
     def getInternalNumber(self, categ_id):
         mad_seq = ""
         if self.type =='service':
-		return mad_seq
+			return mad_seq
         category = self.env['product.category'].search([('id', '=', categ_id)])
         if category:
             if not category.parent_id:
@@ -69,7 +69,8 @@ class Madfox_ProductTemplate(models.Model):
     def getInternalNumber(self, categ_id):
         mad_seq = ""
         if self.type =='service':
-		return mad_seq
+			return mad_seq
+			
         category = self.env['product.category'].search([('id', '=', categ_id)])
         if category:
             if not category.parent_id:
@@ -107,6 +108,3 @@ class Madfox_BaseReport(models.TransientModel):
     company_registry = fields.Char(related='company_id.company_registry', readonly=True)
 
    
-
-
-
