68cf5b90f5f8ae1c2a75a0e6d662accc915555ec
Luse-Chris <96977559+Luse-Chris@users.noreply.github.com>
2023-09-02 01:09:21 +0300

    Delete scan_to_print directory

--- scan_to_print/models/scan_print.py
+++
@@ -1,8 +0,0 @@
-from odoo import api, fields, models, _, tools
-import logging
-
-
-_logger = logging.getLogger(__name__)
-class ScanToPrint(models.Model):
-    _name = 'scan.to.print'
-    _logger.debug("This is my debug message ! ")
