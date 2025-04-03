85d2a740c7473f0306d76f6160d01400f7a58e25
System Administrator <root@192.168.10.42>
2025-04-03 10:06:15 +0500

    partner_vat_number

--- partner-vat-number/models/res_partner.py
+++ partner-vat-number/models/res_partner.py
@@ -1,7 +1,25 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _ 
+from odoo.exceptions import  ValidationError
 
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    @api.constrains('journal_id', 'move_type')
+    def _check_journal_move_type(self):
+        for move in self:
+            if move.is_purchase_document(include_receipts=True) and move.journal_id.type != 'purchase':
+                currency = move.invoice_line_ids[0].currency_id if move.invoice_line_ids else move.currency_id
+                journal_id = self.env['account.journal'].search([
+                    ('type','=','purchase'),
+                    ('currency_id','=',currency.id),
+                ],limit=1)
+                move.journal_id = journal_id.id
+                #raise ValidationError(_("Cannot create a purchase document in a non purchase journal"))
+            if move.is_sale_document(include_receipts=True) and move.journal_id.type != 'sale':
+                raise ValidationError(_("Cannot create a sale document in a non sale journal"))
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
