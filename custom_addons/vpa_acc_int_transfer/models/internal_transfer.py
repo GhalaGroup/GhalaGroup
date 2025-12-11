ded2c4d16915859405a2fec3500c075198e04312
VictorAntipoff <75905706+VictorAntipoff@users.noreply.github.com>
2025-12-11 18:15:25 +0300

    Fix duplicate label warning - rename attachment_count label to 'Attachment Count'

--- custom_addons/vpa_acc_int_transfer/models/internal_transfer.py
+++ custom_addons/vpa_acc_int_transfer/models/internal_transfer.py
@@ -293,7 +293,7 @@ class InternalTransfer(models.Model):
         string='Attachments',
     )
     attachment_count = fields.Integer(
-        string='Attachments',
+        string='Attachment Count',
         compute='_compute_attachment_count',
     )
 
