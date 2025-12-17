410480c70255bb01f98db1a2f49ac79ab4d55fb4
VictorAntipoff <75905706+VictorAntipoff@users.noreply.github.com>
2025-12-17 09:33:31 +0300

    Add wrapper div styling to address section to match ORDER INFO

--- custom_addons/vpa_document_layout/models/vpa_document_template.py
+++ custom_addons/vpa_document_layout/models/vpa_document_template.py
@@ -1531,7 +1531,9 @@ class VPADocumentTemplate(models.Model):
             <tbody>
                 <tr>
                     <td t-if="address" style="width: 50%%; vertical-align: top; padding-right: 20px;">
-                        <t t-out="address"/>
+                        <div style="font-size: 9pt; line-height: 1.8; color: #555;">
+                            <t t-out="address"/>
+                        </div>
                     </td>
                     <td t-if="information_block" style="width: 50%%; vertical-align: top; text-align: right; padding-left: 20px;">
                         <div style="font-size: 9pt; line-height: 1.8; color: #555; text-align: right;">
