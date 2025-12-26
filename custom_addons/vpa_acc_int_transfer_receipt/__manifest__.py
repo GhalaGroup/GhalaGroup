d8af027b376f7497adf2647a7fb82b8541e97f60
VictorAntipoff <75905706+VictorAntipoff@users.noreply.github.com>
2025-12-26 17:07:00 +0300

    Fix VPA report template not loading on upgrade
    - Restore vpa_acc_int_transfer_layout dependency
    - Add VPA template back to data list (loads on upgrade)
    - Remove post_init_hook (only runs on install, not upgrade)

--- custom_addons/vpa_acc_int_transfer_receipt/__manifest__.py
+++ custom_addons/vpa_acc_int_transfer_receipt/__manifest__.py
@@ -50,6 +50,7 @@ Support: info@vpa.co.tz
     'currency': 'USD',
     'depends': [
         'vpa_acc_int_transfer',
+        'vpa_acc_int_transfer_layout',
     ],
     'data': [
         'security/security.xml',
@@ -60,13 +61,11 @@ Support: info@vpa.co.tz
         'views/res_config_settings_views.xml',
         'views/menu_views.xml',
         'report/transfer_report_template.xml',
-        # Note: transfer_report_template_vpa.xml is loaded conditionally via post_init_hook
-        # when vpa_acc_int_transfer_layout module is installed
+        'report/transfer_report_template_vpa.xml',
     ],
     'images': [
         'static/description/icon.png',
     ],
-    'post_init_hook': '_load_vpa_layout_template',
     'installable': True,
     'application': False,
     'auto_install': False,
