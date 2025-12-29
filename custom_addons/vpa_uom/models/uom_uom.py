9a98f490b32b0bdba391a3e187369d4c89002609
VictorAntipoff <75905706+VictorAntipoff@users.noreply.github.com>
2025-12-29 18:27:49 +0300

    Add product count fields and kanban view to UoM management
    - Added product_count computed field to show products using UoM as base
    - Added alt_product_count to show products using UoM as alternative
    - Created kanban view with conversion info and product count badges
    - Added smart button on form to view linked products
    - Added search filters: Reference Units, With Products, Archived

--- custom_addons/vpa_uom/models/uom_uom.py
+++ custom_addons/vpa_uom/models/uom_uom.py
@@ -2,12 +2,49 @@
 # Copyright (C) 2025 VPA Solutions Limited
 # License OPL-1 - See LICENSE file for full copyright and licensing details.
 
-from odoo import api, models, _
+from odoo import api, fields, models, _
 
 
 class UomUom(models.Model):
-    """Extend uom.uom to support quick creation from conversion form."""
+    """Extend uom.uom to support quick creation and product count."""
     _inherit = 'uom.uom'
+
+    # Count of products using this UoM (as base UoM)
+    product_count = fields.Integer(
+        string='Products',
+        compute='_compute_product_count',
+        help="Number of products using this as their base UoM",
+    )
+
+    # Count of products using this as alternative UoM
+    alt_product_count = fields.Integer(
+        string='Alt. Products',
+        compute='_compute_product_count',
+        help="Number of products using this as an alternative UoM",
+    )
+
+    def _compute_product_count(self):
+        for uom in self:
+            # Products using this as base UoM
+            uom.product_count = self.env['product.template'].search_count([
+                ('uom_id', '=', uom.id)
+            ])
+            # Products using this as alternative UoM
+            uom.alt_product_count = self.env['product.uom.conversion'].search_count([
+                ('uom_id', '=', uom.id)
+            ])
+
+    def action_view_products(self):
+        """Open products using this UoM."""
+        self.ensure_one()
+        return {
+            'name': _('Products using %s', self.name),
+            'type': 'ir.actions.act_window',
+            'res_model': 'product.template',
+            'view_mode': 'list,form',
+            'domain': [('uom_id', '=', self.id)],
+            'context': {'default_uom_id': self.id},
+        }
 
     @api.model
     def name_create(self, name):
